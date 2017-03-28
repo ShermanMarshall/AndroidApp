8f77d2797a6389501ea3a2ac10f234c08d947765
Sherman Marshall <sherman.david.marshall@gmail.com>
2017-03-27 22:50:27 -0700

    Consistent/conventional code formatting is nicer

--- SecurityApp/app/src/main/java/com/example/sherman/securityapp/BufferManager.java
+++ SecurityApp/app/src/main/java/com/example/sherman/securityapp/BufferManager.java
@@ -13,14 +13,18 @@ public class BufferManager {
         bitmaps = new int[Constants.BUFFERMAX.n()][Constants.CVIMGSIZE.n()];
         data = new byte[Constants.BUFFERMAX.n()][Constants.CVCOLORCOUNT.n()];
         idx = new int[Constants.BUFFERIDXES.n()];
-        containsData = new boolean[Constants.BUFFERMAX.n()];    }
+        containsData = new boolean[Constants.BUFFERMAX.n()];    
+    }
 
     public byte[] getData() {
         byte[] reference = data[idx[Constants.DATAIDX.n()]];
-        if (idx[Constants.DATAIDX.n()] < Constants.BUFFERMAX.n() - 1)
+        if (idx[Constants.DATAIDX.n()] < Constants.BUFFERMAX.n() - 1){
             idx[Constants.DATAIDX.n()]++;
-        else idx[Constants.DATAIDX.n()] = 0;
-        return reference; }
+        }  else {
+            idx[Constants.DATAIDX.n()] = 0;
+        }
+        return reference; 
+    }
 
     public byte[] dataForNextBitmap() {
         return data[idx[Constants.PARSEIDX.n()]];
@@ -31,16 +35,19 @@ public class BufferManager {
         if (idx[Constants.BMPIDX.n()] < Constants.BUFFERMAX.n() - 1)
             idx[Constants.BMPIDX.n()]++;
         else
-            idx[Constants.BMPIDX.n()] = 0;  }
+            idx[Constants.BMPIDX.n()] = 0;  
+    }
 
     public int[] getBitmap() {
-        return bitmaps[idx[Constants.PARSEIDX.n()]]; }
+        return bitmaps[idx[Constants.PARSEIDX.n()]]; 
+    }
 
     public boolean hasNext() {
         if (containsData[idx[Constants.PARSEIDX.n()]]) {
             return true;
         } else
-            return false;    }
+            return false;    
+    }
 
     public int[] getColors() {
         //containsData[idx[Constants.BMPIDX.n()]] = false;
@@ -54,27 +61,31 @@ public class BufferManager {
 
             setIncomplete();
             return true;
-        } else
+        } else {
             return false;
+        }
     }
 
     public void setIncomplete() {
-        for (int x = 0; x < 3; x++)
+        for (int x = 0; x < 3; x++) {
             complete[x] = false;
+        }
     }
 
     public void next() {
-        if (idx[Constants.PARSEIDX.n()] < Constants.BUFFERMAX.n() - 1)
+        if (idx[Constants.PARSEIDX.n()] < Constants.BUFFERMAX.n() - 1) {
             idx[Constants.PARSEIDX.n()]++;
-        else
+        } else {
             idx[Constants.PARSEIDX.n()] = 0;
+        }
     }
 
     public void prepareNext() {
         bitmaps[idx[Constants.PARSEIDX.n()]] = new int[Constants.CVIMGSIZE.n()];
-        if (idx[Constants.PARSEIDX.n()] < Constants.BUFFERMAX.n() - 1)
+        if (idx[Constants.PARSEIDX.n()] < Constants.BUFFERMAX.n() - 1) {
             idx[Constants.PARSEIDX.n()]++;
-        else
+        } else {
             idx[Constants.PARSEIDX.n()] = 0;
+        }
     }
 }
